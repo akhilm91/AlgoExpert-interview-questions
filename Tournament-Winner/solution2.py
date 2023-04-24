edce2c2a14c4dddc2b719676159976f6091f5734
Akhil Mohan <45547175+akhilm91@users.noreply.github.com>
2023-04-24 16:37:35 +0200

    Create solution2.py

---
+++ Tournament-Winner/solution2.py
@@ -0,0 +1,10 @@
+def tournamentWinner(competitions, results):
+    # Write your code here.
+    winner_map = {}
+    for i in range(len(results)):
+        winner = 1 - results[i]
+        if competitions[i][winner] in winner_map:
+            winner_map[competitions[i][winner]] = winner_map[competitions[i][winner]] + 1
+        else:
+            winner_map[competitions[i][winner]] = 1
+    return max(winner_map, key=winner_map.get)
