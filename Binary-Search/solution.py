411c2acb43e4cd5065233c89103ec3b2882ee0bb
Akhil Mohan <45547175+akhilm91@users.noreply.github.com>
2023-04-26 17:14:19 +0200

    Create solution.py

---
+++ Binary-Search/solution.py
@@ -0,0 +1,12 @@
+def binarySearch(array, target):
+    # Write your code here.
+    left, right = 0, len(array)-1
+    while left <= right:
+        mid = (left+right)//2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            left = mid+1
+        elif array[mid] > target:
+            right = mid-1
+    return -1
