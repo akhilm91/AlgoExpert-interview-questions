5a3a1042a8d65939c354de36403c5a6cf1344fa2
Akhil Mohan <akhilm1009@gmail.com>
2023-05-19 15:53:50 +0200

    updated solution.py

--- Class-Photos/solution.py
+++ Class-Photos/solution.py
@@ -1,1 +1,13 @@
-#test
+def classPhotos(redShirtHeights, blueShirtHeights):
+    # Write your code here.
+    blueShirtHeights.sort()
+    redShirtHeights.sort()
+    if blueShirtHeights[-1] > redShirtHeights[-1]:    
+        for i in range(len(redShirtHeights)):
+            if blueShirtHeights[i] <= redShirtHeights[i]:
+                return False
+    else:
+        for i in range(len(redShirtHeights)):
+            if redShirtHeights[i] <= blueShirtHeights[i]:
+                return False        
+    return True
