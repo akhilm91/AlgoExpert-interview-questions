26abae52dd240aed1abc0704d686ecc6dc85df06
Akhil Mohan <akhilm1009@gmail.com>
2023-05-28 17:19:41 +0200

    added monotonic array

---
+++ Monotonic-Array/solution.py
@@ -0,0 +1,45 @@
+def isMonotonic1(array):
+    isIncr, isDecr = False, False
+    for ptr in range(1, len(array)):
+        if array[ptr] > array[ptr-1]:
+            isIncr = True
+        elif array[ptr] < array[ptr-1]:
+            isDecr = True
+        if isIncr and isDecr:
+            return False
+    return True 
+
+
+def isMonotonic2(array):
+    for i in range(1, len(array)):
+        incr = True
+        if array[i] < array[i-1]:
+            incr = False
+            break
+
+    for i in range(1, len(array)):
+        if incr:
+            if array[i] < array[i-1]:
+                return False
+        else:
+            if array[i] > array[i-1]:
+                return False
+    return True   
+
+
+def isMonotonic3(array):
+    final = False
+    for i in range(1, len(array)):
+        if final == False:
+            if array[i] > array[i-1]:
+                incr, final = True, True
+            elif array[i] < array[i-1]:
+                incr, final = False, True
+        else:
+            if incr:
+                if array[i] < array[i-1]:
+                    return False
+            else:
+                if array[i] > array[i-1]:
+                    return False      
+    return True
